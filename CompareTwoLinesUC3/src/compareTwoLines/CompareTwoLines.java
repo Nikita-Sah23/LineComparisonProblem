be0e8a50ebae50e30c15bfe0ec5449cfe00cfc75
Nikita-sah <nikitasah4gmail.com>
2022-08-23 17:47:44 +0530

    Second commit

--- CompareTwoLinesUC3/src/compareTwoLines/CompareTwoLines.java
+++ CompareTwoLinesUC3/src/compareTwoLines/CompareTwoLines.java
@@ -3,48 +3,48 @@ package compareTwoLines;
 public class CompareTwoLines {
 
 	public static void main(String[] args) {
-		int x1 = 8;
-		int x2 = 16;
-		int y1 = 8;
-		int y2 = 16;
+		int x1 = 80;
+		int x2 = 40;
+		int y1 = 80;
+		int y2 = 40;
 		
-		int a1 = 8;
-		int a2 = 16;
-		int b1 = 8;
-		int b2 = 16;
+		int a1 = 80;
+		int a2 = 400;
+		int b1 = 80;
+		int b2 = 400;
 	
-		String z1 ="0";
-		String z2 ="0";
+		Double z1 =0.0;
+		Double z2 =0.0;
 				
 		float Line1_Length =  (float) Math.sqrt( Math.pow (x2 - x1 ,2) + Math.pow (y2- y1 , 2));
 			
-		z1 =String.valueOf(Line1_Length);  
+		z1 =Double.valueOf(Line1_Length);  
 		
 		System.out.println( "Line1_Length" );
 		System.out.println( Line1_Length );
 		
 		float Line2_Length =  (float) Math.sqrt( Math.pow (a2 - a1 ,2) + Math.pow (b2- b1 , 2));
 		
-		z2 =String.valueOf(Line2_Length);
+		z2 =Double.valueOf(Line2_Length);
 		
 		System.out.println( "Line2_Length");
 		System.out.println( Line2_Length );
 		
-		System.out.println(z1);
-		System.out.println(z2);
+		//System.out.println(z1);
+		//System.out.println(z2);
 		
-		System.out.println(z1.compareTo(z2)); // if z2--> greater thn -ve , if z1-->greater thn +ve value
+    	System.out.println(z1.compareTo(z2)); // if z2--> greater thn -ve , if z1-->greater thn +ve value
 		
-	 if (z1.compareTo(z2) > 0)
+	 if (z1.compareTo(z2) == 1)
 				
 	{
-		System.out.println(z1.compareTo(z2));				
+		// System.out.println(z1.compareTo(z2));				
 			System.out.println("z1 line is greater");	
 	}
 		
-		 else if (z1.compareTo(z2) < 0)
+		 else if (z1.compareTo(z2) ==-1)
 		{
-			System.out.println(z1.compareTo(z2));
+			//System.out.println(z1.compareTo(z2));
 			System.out.println("z2 Line is greater");
 		}
 		else 
